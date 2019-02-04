cade8cc35f6e964b64b7e1b3a08e35005b8fe4f8
Khateeb <khateeb321@gmail.com>
2019-02-04 14:02:01 +0100

    Cutome Pipe (filters) & Shared module concept

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CustomersComponent }  from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
-  imports:      [ CommonModule ],
+  imports:      [ CommonModule, SharedModule ],
   declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ],
   exports: [ CustomersComponent ]
 })
